7f7909aa06b234f2bf8a2da82346f16996e4051c
MoHpaK <dsv0810@gmail.com>
2025-04-28 19:49:32 +0400

    Add files via upload

---
+++ Ptch_First/FasterBuilding/Harmony/FasterBuilding.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+using HarmonyLib;
+
+internal class Dayuppy_BuildIntervalPatches
+{
+    [HarmonyPatch]
+    public class GameManagercreateWorldBuildInterval
+    {
+        private static IEnumerable<MethodInfo> TargetMethods()
+        {
+            yield return AccessTools.EnumeratorMoveNext(AccessTools.Method(typeof(GameManager), "createWorld"));
+        }
+
+        public static void Postfix(GameManager __instance)
+        {
+            Constants.cBuildIntervall = 0.35f;
+        }
+    }
+}
