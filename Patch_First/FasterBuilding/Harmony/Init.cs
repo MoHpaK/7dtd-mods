c09688b11b2cea234ee1129a44d43de613b82bfd
MoHpaK <dsv0810@gmail.com>
2025-04-28 21:58:36 +0400

    Add files via upload

---
+++ Patch_First/FasterBuilding/Harmony/Init.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+using HarmonyLib;
+
+public class FasterBuilding : IModApi
+{
+    public void InitMod(Mod _modInstance)
+    {
+        Harmony harmony = new Harmony(GetType().ToString());
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
